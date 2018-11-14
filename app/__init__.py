9865b0d8d0589737bf6015e62b8803befca63b78
ying liu <lychiyu@gmail.com>
2018-11-14 08:47:32 +0800

    home 页面搭建

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from app.admin import admin as admin_blueprint
 from app.home import home as home_blueprint
 
@@ -7,3 +7,8 @@ app.debug = True
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('home/404.html'), 404
--- app/home/views.py
+++ app/home/views.py
@@ -1,6 +1,57 @@
 from app.home import home
+from flask import render_template, redirect, url_for
 
 
 @home.route("/")
 def index():
-    return "home"
+    return render_template('home/index.html')
+
+
+@home.route("/animation/")
+def animation():
+    return render_template('home/animation.html')
+
+
+@home.route("/play/")
+def play():
+    return render_template('home/play.html')
+
+
+@home.route("/login/")
+def login():
+    return render_template('home/login.html')
+
+
+@home.route("/logout/")
+def logout():
+    return redirect(url_for('home.login'))
+
+
+@home.route("/register/")
+def register():
+    return render_template('home/register.html')
+
+
+@home.route("/user/")
+def user():
+    return render_template('home/user.html')
+
+
+@home.route("/pwd/")
+def pwd():
+    return render_template('home/pwd.html')
+
+
+@home.route("/comments/")
+def comments():
+    return render_template('home/comments.html')
+
+
+@home.route("/login_log/")
+def login_log():
+    return render_template('home/login_log.html')
+
+
+@home.route("/collection/")
+def collection():
+    return render_template('home/collection.html')
